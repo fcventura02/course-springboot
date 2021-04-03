bbb0110ee563f7a0b03c706d59f34e36570c2c25
fcventura02 <filipecvr4@gmail.com>
2021-04-03 18:46:00 -0300

    Service layer, component registration

---
+++ src/main/java/com/devventura/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.devventura.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devventura.course.entities.User;
+import com.devventura.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
