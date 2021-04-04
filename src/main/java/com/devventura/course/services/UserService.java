27129737363de0085c3dac24f776f580b7216be0
fcventura02 <filipecvr4@gmail.com>
2021-04-04 19:34:42 -0300

    Exception handling - update

--- src/main/java/com/devventura/course/services/UserService.java
+++ src/main/java/com/devventura/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.devventura.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -21,35 +23,39 @@ public class UserService {
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		try {
-		repository.deleteById(id);
+			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User user) {
-		User entity = repository.getOne(id);
-		updateData(entity, user);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, user);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
 		entity.setName(user.getName());
 		entity.setEmail(user.getEmail());
-		entity.setPhone(user.getPhone());		
+		entity.setPhone(user.getPhone());
 	}
 }
